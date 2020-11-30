d7742b6da8419c65f0ce47e91a7c3a69c1474bf7
Leonardo Capogna <leonardocapogna@gmail.com>
2020-11-30 16:19:49 +0100

    list

--- js/script.js
+++ js/script.js
@@ -4,14 +4,26 @@
 $(document).ready(function() {
 
   var list = [
-    text : "colazione",
-    text : "Lezione della mattina",
-    text : "pranzare",
-    text : "esercitazione pomeriggio",
+   {
+    text:"colazione",
+  },
+   {
+    text:"Lezione della mattina",
+  },
+   {
+    text:"pranzare",
+  },
+   {
+    text:"esercitazione pomeriggio",
+  },
   ];
 
   var source = $("#entry-template").html();
   var template = Handlebars.compile(source);
 
+  for( var = 0; i < list.length; i++){
+    var html = template(list[i]);
+    $(".todo-list").append(html);
+  }
 
 });

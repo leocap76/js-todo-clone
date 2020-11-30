f01269f5015b3d1fd401ee7849f22fb8e6f1bda6
Leonardo Capogna <leonardocapogna@gmail.com>
2020-11-30 16:09:05 +0100

    start repo

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// console.log('prova');
+
+
+$(document).ready(function() {
+
+  var list = [
+    text : "colazione",
+    text : "Lezione della mattina",
+    text : "pranzare",
+    text : "esercitazione pomeriggio",
+  ];
+
+  var source = $("#entry-template").html();
+  var template = Handlebars.compile(source);
+
+
+});
